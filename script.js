0acda1cdba2f07901a189cc9c6e152cb469258cd
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-11-05 22:16:03 +0000

    Wrote first container for React to show the image adopt me instead of the text

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 import React from "react";
 
-const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
-};
-
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
